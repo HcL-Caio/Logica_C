e928a9cda14fb17dec03f09b103a3320dc37fb39
DarkChild <caiopcmx@gmail.com>
2025-05-29 10:39:34 -0300

    Add files via upload

---
+++ Atv.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int day = 1;
+
+switch (day) {
+ case 1:
+    printf("Domingo anoite");
+    break;
+ case 2:
+    printf("Segunda");
+    break;
+ case 3:
+    printf("Terça");
+    break;
+ default:
+    printf("Fudeo");
+break;
+    }
+    }
